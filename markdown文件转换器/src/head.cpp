46cabeb094d34e0510f6b6d473705b68416d8fdc
陈朝臣 <9062498+chen-chaochen@user.noreply.gitee.com>
2022-08-02 02:51:06 +0000

    删除文件 markdown文件转换器

--- markdown文件转换器/src/head.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <cstring>
-using namespace std;
-string _mdfile,_htmfile;
--- markdown文件转换器/src/keyword.cpp
+++
@@ -1,4 +0,0 @@
-// HTML 前置标签
-const std::string frontTag[] = { "", "<p>", "", "<ul>", "<ol>", "<li>", "<em>", "<strong>", "<hr color=#CCCCCC size=1 / > ", "", "<blockquote>", "<h1>", "<h2>", "<h3>", "<h4>", "<h5>", "<h6>", "<pre><code>", "<code>" };
-// HTML 后置标签
-const std::string backTag[] = {"", "</p>", "", "</ul>", "</ol>", "</li>", "</em>",	"</strong>", "", "", "</blockquote>", "</h1>", "</h2>",	"</h3>", "</h4>", "</h5>", "</h6>", "</code></pre>", "</code>" };
