ff716a39f3397d3f114e26406be154a4e82b3ea1
陈朝臣 <9062498+chen-chaochen@user.noreply.gitee.com>
2022-08-03 07:57:51 +0000

    发行版0.2.2

---
+++ losu0.2/linux/lib/losuvm_rand.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#define random(a,b) (rand()%(b + 1 -a)+a) 
+
+using namespace std;
+extern "C"{
+
+const char* ls_rand(const char* _begin,const char* _end)
+{
+	clock_t _t;
+	_t = clock();
+    srand(_t);  
+    return to_string(random(atoi(_begin),atoi(_end))).c_str();
+}
+}
